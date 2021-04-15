87e67d362509738c0433457a2dd564e3ad60126d
sinnrrr <dimasoltusyuk@gmail.com>
2021-04-16 00:22:55 +0300

    🎭 Bugfixes + panel navigation + models update

--- app/posts/mutations/createPost.ts
+++ app/posts/mutations/createPost.ts
@@ -1,10 +1,10 @@
 import { resolver } from "blitz"
-import db from "db"
+import db, { PostType } from "db"
 import { CreatePost } from "../validations"
 
 export default resolver.pipe(resolver.zod(CreatePost), resolver.authorize(), async (input) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const post = await db.post.create({ data: input })
+  const post = await db.post.create({ data: { ...input, type: input.type as PostType } })
 
   return post
 })
