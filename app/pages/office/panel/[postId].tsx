560b44e9c5da4a9dff78952cebe8b36240a811e2
sinnrrr <dimasoltusyuk@gmail.com>
2021-04-19 22:37:39 +0300

    🎉 v0.0.7: SEO

--- app/pages/office/panel/[postId].tsx
+++ app/pages/office/panel/[postId].tsx
@@ -9,37 +9,51 @@ import getPost from "app/posts/queries/getPost"
 import { BiImage } from "react-icons/bi"
 import { usePostRedirect } from "app/core/hooks/usePostRedirect"
 import MiddlePanel from "app/core/components/MiddlePanel"
+import { NextSeo } from "next-seo"
 
 const Quick: BlitzPage = () => {
   const postRedirect = usePostRedirect()
   const [post] = useQuery(getPost, { id: useParam("postId", "number") })
 
   return (
-    <VStack align="flex-start">
-      <MiddlePanel heading="Оголошення" />
+    <>
+      <NextSeo
+        title={`${post.title} | Loonify`}
+        description={"Оголошення на бюро знахідок"}
+        openGraph={{
+          url: window.location.href && window.location.href,
+          title: `${post.title} | Loonify`,
+          description: "Оголошення на бюро знахідок",
+          images: post.images.map((image) => ({ url: image })),
+          site_name: "Loonify",
+        }}
+      />
+      <VStack align="flex-start">
+        <MiddlePanel heading="Оголошення" />
 
-      {post.images.length > 0 ? (
-        <Image src={post.images[0]} />
-      ) : (
-        <Box>
-          <BiImage size="100%" />
-        </Box>
-      )}
-      <Text fontWeight="bold" fontSize={30}>
-        {post.title}
-      </Text>
-      <Text>{post.description}</Text>
-      <Alert status="info">
-        <AlertIcon />
-        <Text>
-          Ви бачите скорочену версію оголошення. Щоб мати більше можливостей варто перейти до повної
-          версії.
+        {post.images.length > 0 ? (
+          <Image src={post.images[0]} />
+        ) : (
+          <Box>
+            <BiImage size="100%" />
+          </Box>
+        )}
+        <Text fontWeight="bold" fontSize={30}>
+          {post.title}
         </Text>
-      </Alert>
-      <Button onClick={() => postRedirect(post.id)} isFullWidth>
-        Дивитись повне оголошення
-      </Button>
-    </VStack>
+        <Text>{post.description}</Text>
+        <Alert status="info">
+          <AlertIcon />
+          <Text>
+            Ви бачите скорочену версію оголошення. Щоб мати більше можливостей варто перейти до
+            повної версії.
+          </Text>
+        </Alert>
+        <Button onClick={() => postRedirect(post.id)} isFullWidth>
+          Дивитись повне оголошення
+        </Button>
+      </VStack>
+    </>
   )
 }
 
