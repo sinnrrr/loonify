c320428f06272ce6e423e3d1a51877f6db12959a
sinnrrr <dimasoltusyuk@gmail.com>
2021-05-09 01:07:16 +0300

    🎉 v0.0.9: Ok

--- app/api/v0/posts/[id]/pdf.ts
+++ app/api/v0/posts/[id]/pdf.ts
@@ -1,18 +1,17 @@
 import { BlitzApiRequest, BlitzApiResponse } from "@blitzjs/core"
-import puppeteer from "puppeteer"
+import { chromium } from "playwright"
 
 const generatePdf = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
-  const browser = await puppeteer.launch()
+  const browser = await chromium.launch()
   const page = await browser.newPage()
 
   await page.goto(
     (process.env.NODE_ENV === "production"
       ? "https://loonify.rocks"
-      : `http://localhost:${process.env.PORT}`) + `/office/posts/${req.query.id}/pdf`,
-    {
-      waitUntil: "networkidle2",
-    }
+      : `http://localhost:${process.env.PORT}`) + `/office/posts/${req.query.id}/pdf`
   )
+
+  await page.waitForLoadState("networkidle")
 
   const pdf = await page.pdf({ format: "a4" })
 
