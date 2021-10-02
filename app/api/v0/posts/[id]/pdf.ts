54bad68bbe67039eb2500a97227b9866163b7776
Dmytro <dimasoltusyuk@gmail.com>
2021-10-02 14:03:27 +0300

    Rendering url change

--- app/api/v0/posts/[id]/pdf.ts
+++ app/api/v0/posts/[id]/pdf.ts
@@ -13,7 +13,7 @@ const generatePdf = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
 
   await page.goto(
     (process.env.NODE_ENV === "production"
-      ? "https://loonify.rocks"
+      ? "https://loonify.vercel.app"
       : `http://localhost:${process.env.PORT}`) + `/office/posts/${req.query.id}/pdf`,
     {
       waitUntil: "networkidle2",
