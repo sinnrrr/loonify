c083bbabe97d3e4ed2d7331e8fa7c3d457a70a9f
Dmytro <dimasoltusyuk@gmail.com>
2021-10-24 16:49:27 +0300

    fix: pdf generation on local environment

--- app/api/v0/posts/[id]/pdf.ts
+++ app/api/v0/posts/[id]/pdf.ts
@@ -6,7 +6,7 @@ const generatePdf = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+    headless: true,
     ignoreHTTPSErrors: true,
   })
   const page = await browser.newPage()
